85f581af084f71e436c5a42478be77cd86562ebf
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-03-25 19:35:43 +0000

    Updates

--- jla-portfolio/next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // Use the prefix for the GitHub Pages repository
-  basePath: process.env.NODE_ENV === "production" ? "/Portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Portfolio" : "",
-};
