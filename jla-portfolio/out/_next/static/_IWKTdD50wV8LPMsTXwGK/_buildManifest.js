78f24a2434cbeff61263404bf7aae772547d68c7
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-03-25 06:05:15 +0000

    commit changes

--- jla-portfolio/out/_next/static/_IWKTdD50wV8LPMsTXwGK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/637-c7b5f44af288becf.js","static/css/cdb4e56caa44fa59.css","static/chunks/pages/index-94f742c6cf3d8e00.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
