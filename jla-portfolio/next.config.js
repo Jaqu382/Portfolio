ee3f4240115cde96aea320ebe0aea51a2546d956
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-03-26 16:37:12 +0000

    commit with gh-pages updates

--- jla-portfolio/next.config.js
+++ jla-portfolio/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  basePath: isProd ? '/Portfolio' : '',
+  assetPrefix: isProd ? '/Portfolio/' : '',
+};
