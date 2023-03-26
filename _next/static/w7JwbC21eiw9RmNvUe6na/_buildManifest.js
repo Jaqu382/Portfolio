fcc6e9422346172baeb9a39544eafe9599777bac
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-03-26 16:34:32 +0000

    Updates

---
+++ _next/static/w7JwbC21eiw9RmNvUe6na/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/637-c7b5f44af288becf.js","static/css/5f383e69b35abbb6.css","static/chunks/pages/index-68f61beb3ea294ca.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
